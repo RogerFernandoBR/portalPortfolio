2f079c5317752cdfc51364f19b8a326d713fe5d7
Roger Fernando de Oliveira Carvalho <rogerfernandodeoliveira@gmail.com>
2023-06-16 21:26:23 -0300

    aboutPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,7 +41,8 @@ import { HardSkillsComponent } from './components/hard-skills/hard-skills.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
